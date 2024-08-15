879d977b3e14902b840750931ba85b1f48e2a458
Jedidiah dennis <thejedidiahdennis@gmail.com>
2024-08-15 10:23:27 -0400

    add liking and disliking functionality.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
+import { useState } from 'react';
 import Card from './components/Card';
+import my_config from './config.js';
 
 function App() {
+  function likeUser() {
+    console.log(card_data.username,' has been liked.');
+  }
+  function dislikeUser() {
+    console.log(card_data.username, ' has been disliked.');
+  }
+  const [card_data, setCardData] = useState(getNewUserData);
+  async function getNewUserData() {
+    const url = 'https://api.api-ninjas.com/v1/randomuser';
+    const my_key = my_config.MY_API_KEY;
+    const my_header = new Headers();
+    my_header.append('X-Api-Key', my_key)
+    try {
+      const response = await fetch(url, {headers: my_header});
+      if (response.ok) {
+        const JSONresponse = await response.json();
+        setCardData(JSONresponse);
+      } else {
+        console.log('Error: ', response);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="container">
       <div className="container">
         <div className="py-2 my-1 fs-1 text-center">WebTItle!</div>
 
         <div className="d-flex justify-content-around">
-          <button type="button" class="btn align-self-center btn-danger fs-1">
+          <button
+            type="button"
+            className="btn align-self-center btn-danger fs-1"
+            onClick={()=>{dislikeUser(); getNewUserData()}}
+          >
             &#9747;
           </button>
-          <Card classess={'my-5'}></Card>
-          <button type="button" class="btn align-self-center btn-success fs-1">
+          <Card
+            classess={'my-5'}
+            title={card_data.username}
+            text={card_data.address}
+          ></Card>
+          <button
+            type="button"
+            className="btn align-self-center btn-success fs-1"
+            onClick={()=>{likeUser(); getNewUserData()}}
+          >
             &#10003;
           </button>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
-function Card({classess}) {
+function Card({classess, title, text}) {
   
   return (
     <div className={"card shadow " + classess}  style={{width: '24rem'}}>
       <img src="https://placehold.co/400" className="card-img-top p-2" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{title}</h5>
         <p className="card-text text-wrap">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {text}
         </p>
       </div>
     </div>
